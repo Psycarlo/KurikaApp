8eafbff803eb5a7d88c8f841ec2811b28c2966c4
Psycarlo <karkosyk@gmail.com>
2020-02-02 23:22:59 +0000

    Sample TDD Progress 7
    Part 9 of Resocoder's tutorial

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:kurika_app/core/error/exceptions.dart';
+import 'package:meta/meta.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
@@ -9,4 +15,37 @@ abstract class NumberTriviaRemoteDataSource {
   /// 
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  NumberTriviaRemoteDataSourceImpl({
+    @required this.client
+  });  
+
+  final http.Client client;
+  
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) {
+    return _getTriviaFromUrl('http://numbersapi.com/$number');
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    return _getTriviaFromUrl('http://numbersapi.com/random');
+  }
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      url,
+      headers: {'Content-Type': 'application/json'}
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(
+        json.decode(response.body) as Map<String, dynamic>
+      );
+    } else {
+      throw ServerException();
+    }
+  }
 }
