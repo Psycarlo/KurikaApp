ecbab0272a547b7f23d145722d64404bdee6a31a
Psycarlo <karkosyk@gmail.com>
2020-02-12 23:28:09 +0000

    Restart

--- lib/core/error/exceptions.dart
+++
@@ -1,6 +0,0 @@
-// Can create field String message to get more information
-// about the exceptions
-
-class ServerException implements Exception {}
-
-class CacheException implements Exception {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
-import 'injection_container.dart' as di;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Number Trivia',
-      theme: ThemeData(
-        primaryColor: Colors.green.shade800,
-        accentColor: Colors.green.shade600
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
       ),
-      home: NumberTriviaPage(),
     );
   }
 }
